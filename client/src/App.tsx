e43037c1c2a2b457b4edd8aa582e2f3785d6488f
“[Harsh <[harshmalik2410@gmail.com]>
2025-02-02 22:48:18 +0530

    landing page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,9 +13,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/builder" element={<Builder />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/Profile" element={<Profile />} />
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -17,19 +17,30 @@
 //     </div>
 //   );
 // }
-
+import { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import  HoverBorderGradient  from "./ui/hover-border-gradient";
 export default function Navbar() {
+  const [login,Setlogin]=useState(true)
   return (
-    <div className="bg-[#47128c] p-4 flex justify-between items-center w-full fixed top-0">
-      <h1 className="text-white text-xl">MyApp</h1>
+   
       <Link
-        to="/Login"
-        className="bg-white text-[#47128c] px-4 py-2 rounded-lg shadow-md hover:bg-gray-200 transition"
+        to={login?"profile":"login"}
       >
-        Sign In
+        <div className="m-40 flex justify-center text-center absolute -top-[19%] -right-[8%]">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            <span>
+              {
+               login?"Profile":"Login"
+              }
+             
+            </span>
+          </HoverBorderGradient>
+        </div>
       </Link>
-    </div>
   );
 }
